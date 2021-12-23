e5d3df3547a622476092d3c1aa92b07f32dd1fee
Mario <mamerida2013@gmail.com>
2021-12-23 15:13:59 -0300

    ejemplo de re-renderizacion atravez de componentes padres e hijos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import { Component } from "react";
 
+
+class Button extends Component{
+  
+  render(){
+    // al estar button dentro de app al executar render de app se ejecuta tambien el render de button por que el componente hijo 
+    // se actualiza con el padre
+    console.log("se renderizo boton")
+    return(
+      <button>
+        Enviar
+      </button>
+    )
+  }
+}
+
 // en este caso creo una clase App que hago exnteder el componramiento de component 
 class App extends Component{
 
@@ -7,7 +22,7 @@ class App extends Component{
     //esta condificona en su modificacion cuando se modifica render por esto la funcion setState. Que al colocar un nuevo valor con setState 
     //vuelve a correr render
     // render se actualiza cuando se ejecuta setState o el componente padre se renderiza 
-    //state palabra reservada solo para estado
+    //state palabra reservada solo para estado 
     state={
       valor:3
     }
@@ -18,8 +33,9 @@ class App extends Component{
       return(
         <div>
           <p>Hola Mundo</p>
-          <button className="{}" onClick={() =>this.setState({valor: 2})}>
-            Enviar
+          <Button/>
+          <button className={`${this.state.valor}`} onClick={() =>this.setState({valor: 2})}>
+            Enviar en app
           </button>
         </div>
       )
