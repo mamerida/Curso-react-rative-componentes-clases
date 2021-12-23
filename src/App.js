ee2b25bc898498ec7879279413c88f17d56e225d
Mario <mamerida2013@gmail.com>
2021-12-23 15:48:33 -0300

    trabajando con los estados de los componentes y con componentDidMount

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import { Component } from "react";
 
 
 class Button extends Component{
-  
+  state={}
+  //funcion que siempre se va a ejecutar en el momento de crear el componente
+  constructor(props){
+    super(props) // refierendo al componente padre Component se le pasan las propiedades al constructor 
+    console.log("constructor",props)
+    
+  }
+
+  //se llama al querer actualiar el estado del componente
+  //se suele utilizar para renderizacion condicional en el front con valores del DOM
+  //llamados de datos a una api antes de usar el component
+  //se ejecuta despues del constructor y del render por ende existe en el DOM y se pueden manipular sus variables
+  componentDidMount(){
+    console.log("componentDidMount")
+  }
+
+
   render(){
     // al estar button dentro de app al executar render de app se ejecuta tambien el render de button por que el componente hijo 
     // se actualiza con el padre
@@ -33,7 +49,7 @@ class App extends Component{
       return(
         <div>
           <p>Hola Mundo</p>
-          <Button/>
+          <Button chanchito ="Feliz"/>
           <button className={`${this.state.valor}`} onClick={() =>this.setState({valor: 2})}>
             Enviar en app
           </button>
@@ -45,3 +61,24 @@ class App extends Component{
 
 
 export default App;
+
+
+// Estado de vida de los componentes 
+// componentDidMount accione que se realizan cuando se crea el componente
+// componentDidUpdate acciones que se realizan cuando se re-renderiza el componente
+// componentWillUnmpunt acciones a realizar cuando el componente se va a eliminar 
+
+
+// class Button extends Component{
+  
+//   render(){
+//     // al estar button dentro de app al executar render de app se ejecuta tambien el render de button por que el componente hijo 
+//     // se actualiza con el padre
+//     console.log("se renderizo boton")
+//     return(
+//       <button>
+//         Enviar
+//       </button>
+//     )
+//   }
+// }
